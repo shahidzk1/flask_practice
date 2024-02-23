2b07371f18712c3a703c63fc94109f56c80a6d50
Shahid <shahid.khan@uni-tuebingen.de>
2024-02-23 11:18:12 +0100

    added the method of index_explicit in server.py

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask,  make_response
 # create Flask application by initializing the Flask class
 app = Flask(__name__)
 #create first route
 @app.route("/")
-#method for the main root URL
+#method for the main root URL 
 def index():
     #return html
     return "hello world"
     #return json
     #return "message": "Hello World"
 
+#In the index method Flask automatically sends an HTTP 200 OK successful response when one sent back a message
+#However, one can also set the return status explicitly
 # URL of /no_content
 @app.route("/no_content")
 def no_content():
@@ -19,4 +21,10 @@ def no_content():
     """
     return ({"message":"No content found"}, 204)
 
+#Send custom HTTP code back with the make_response() method
+@app.route("/exp")
+def index_explicit():
+    resp = make_response({"message":"Hello"})
+    resp.status_code = 200
+    return resp
 
