6a97e301eda9c716859696fffcbe0a795507e6ad
Shahid <shahid.khan@uni-tuebingen.de>
2024-02-23 08:59:16 +0100

    added the app in server.py

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+# create Flask application by initializing the Flask class
+app = Flask(__name__)
+#create first route
+@app.route("/")
+#method for the main root URL
+def index():
+    #return html
+    return "hello world"
