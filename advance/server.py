0ae08f7f5078eee0f7779a05f220830e42a72472
Shahid khan <shahidzafarkhan@gmail.com>
2024-03-07 03:38:24 -0500

    corrected path for advnaved exercise

---
+++ advance/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from calculat.maths import sum, subtract, multiply
+
+app = Flask("Mathematics Problem Solver")
+
+@app.route("/sum")
+def sum_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    return str(sum(num1, num2))
+
+@app.route("/sub")
+def sub_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    return str(subtract(num1, num2))
+
+@app.route("/mul")
+def mul_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    return str(multiply(num1, num2)) 
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
